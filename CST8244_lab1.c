1375473d4c7f0edbb6e467c7eed3dff19a36a78b
kain0012 <kain0012@algonquinlive.com>
2016-09-26 16:20:24 -0400

    first commit

---
+++ CST8244_lab1.c
@@ -0,0 +1,7 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+	printf("Welcome to the QNX Momentics IDE for people who don't read good.\n");
+	return EXIT_SUCCESS;
+}
